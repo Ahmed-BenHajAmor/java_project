46cd5e6b7dc597d93d71561f5e5c1dc546903fef
David <nnassi377@gmail.com>
2024-12-02 20:26:47 +0100

    Classes ready

--- Formateur/Formateur.java
+++ Formateur/Formateur.java
@@ -1,7 +1,18 @@
 package Formateur;
 
+import java.util.Vector;
+
+import Formation.Formation;
 import Utilisateur.Utilisateur;
 
 public class Formateur extends Utilisateur {
-    vector formations;
+    Vector<Formation> formations;
+
+    public Formateur(String nom, String email, String motDePasse){
+        super(nom, email, motDePasse);
+
+    }
+    void ajouterFormation(Formation formation){
+        formations.add(formation);
+    }
 }
